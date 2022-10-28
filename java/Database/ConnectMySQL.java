9a5b8c9bda366e4c186d8464ccc2e2110ac22072
Figueredo <jhowfigueredo2011@hotmail.com>
2022-10-28 13:56:09 -0300

    Create Connection with Database

---
+++ java/Database/ConnectMySQL.java
@@ -0,0 +1,38 @@
+
+// cSpell: disable
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectMySQL {
+    // Para uso com Postgres, faça somente a alteração dos campos url, user e password.
+    // Postgres: url = jdbc:postgresql://localhost:3306/clients
+    // MySQL: url = jdbc:mysql://localhost:3306/clients
+    // Mariadb: url = jdbc:mysql://localhost:3306/clients
+    
+    static String driverMySQL = "org.gjt.mysql.Driver";
+    static String url = "jdbc:mysql://localhost:3306/clients";
+    static String user = "admin";
+    static String password = "admin";
+
+    public static void main(String[] args) {
+        try {
+            
+            System.out.println("Iniciando o JDBC");
+            Class.forName(driverMySQL);
+            Connection connection = DriverManager.getConnection(url, user, password); 
+            ResultSet consult_tables = connection.createStatement().executeQuery("SELECT * FROM Clients");
+
+            while (consult_tables.next()) {
+                System.out.println("Nome: " + consult_tables.getString("nome"));
+            }
+            connection.close();
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Driver não localizado, verifique novamente");
+        } catch (SQLException e) {
+
+            System.out.println("Erro ao acessar banco " + e.getMessage());
+    }
+}}
